62f7bb72d2aa54ef5eb24ba2e26ec441cd236b5c
candra kriswinarto <ckriswinarto@gmail.com>
2023-01-24 08:52:43 +0800

    change the api url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = ({ setListImages, setIsLoading }) => {
 
     try {
       setIsLoading(true);
-      const response = await fetch("http://localhost:5500/openai/images", {
+      const response = await fetch("https://genieart-api-production.up.railway.app/openai/images", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
